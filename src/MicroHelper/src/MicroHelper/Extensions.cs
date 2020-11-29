febdee4d9ed70dd5976847683b0b882fd8b4a6d1
Dominik Gogola <d.s.gogola@gmail.com>
2020-11-29 22:50:10 +0100

    - added packages and method for getting data from app settings

---
+++ src/MicroHelper/src/MicroHelper/Extensions.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+namespace MicroHelper
+{
+    public static class Extensions
+    {
+        public static T GetOptions<T>(this IConfiguration configuration, string sectionName)
+            where T : new()
+        {
+            var model = new T();
+            configuration.GetSection(sectionName).Bind(model);
+            return model;
+        }
+    }
+}
