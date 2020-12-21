b9f896e7ad46369535b01c701f79f84b1b6f29e1
Dominik Gogola <d.s.gogola@gmail.com>
2020-12-21 22:30:56 +0100

    - added missing classes and implementations around mongo

--- src/MicroHelper.Database.MongoDB/src/MicroHelper.Database.MongoDB/Extensions.cs
+++ src/MicroHelper.Database.MongoDB/src/MicroHelper.Database.MongoDB/Extensions.cs
@@ -1,6 +1,63 @@
-﻿namespace MicroHelper.Database.MongoDB
+﻿using MicroHelper.Database.MongoDB.Interfaces;
+using MicroHelper.Types;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using System;
+
+namespace MicroHelper.Database.MongoDB
 {
     public static class Extensions
     {
+        public static IServiceCollection AddMongo(this IServiceCollection services, MongoDbOptions mongoOptions, bool registerConventions = true)
+        {
+
+            services.AddSingleton(mongoOptions);
+            services.AddSingleton<IMongoClient>(sp =>
+            {
+                var options = sp.GetService<MongoDbOptions>();
+                return new MongoClient(options.ConnectionString);
+            });
+            services.AddTransient(sp =>
+            {
+                var options = sp.GetService<MongoDbOptions>();
+                var client = sp.GetService<IMongoClient>();
+                return client.GetDatabase(options.Database);
+            });
+
+            if (registerConventions)
+                RegisterConventions();
+
+            return services;
+        }
+
+        private static void RegisterConventions()
+        {
+            BsonSerializer.RegisterSerializer(typeof(decimal), new DecimalSerializer(BsonType.Decimal128));
+            BsonSerializer.RegisterSerializer(typeof(decimal?),
+                new NullableSerializer<decimal>(new DecimalSerializer(BsonType.Decimal128)));
+            ConventionRegistry.Register("convey", new ConventionPack
+            {
+                new CamelCaseElementNameConvention(),
+                new IgnoreExtraElementsConvention(true),
+                new EnumRepresentationConvention(BsonType.String),
+            }, _ => true);
+        }
+
+        public static IServiceCollection AddMongoRepository<TEntity, TIdentifiable>(this IServiceCollection services,
+            string collectionName)
+            where TEntity : IIdentifiable<TIdentifiable>
+        {
+            services.AddTransient<IMongoRepository<TEntity, TIdentifiable>>(sp =>
+            {
+                var database = sp.GetService<IMongoDatabase>();
+                return new MongoRepository<TEntity, TIdentifiable>(database, collectionName);
+            });
+
+            return services;
+        }
     }
 }
